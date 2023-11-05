1cdd18840fa58e01f2d4c63f020940d664d42fb8
Jay-SCM <John.Farmer1086@gmail.com>
2023-11-06 01:01:44 +0930

    added styles linked the rst of the html fixed the navbar and offcanvas, added js for dresses shirts shoes skirts pants and bags - added the login page and the fixed the broken search box

--- JavaScript/cart.js
+++ JavaScript/cart.js
@@ -1,17 +1,17 @@
 let ShoppingCart = document.getElementById("shopping-cart");
 let label = document.getElementById("label");
 
-
-// Basket to hold all the selected items
-// the getItem part is retrieving data from the local storage
-// if local storage is blank, basket becomes an empty array
-
+/**
+ * ! Basket to hold all the selected items
+ * ? the getItem part is retrieving data from the local storage
+ * ? if local storage is blank, basket becomes an empty array
+ */
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-
- //To calculate total amount of selected Items
-
+/**
+ * ! To calculate total amount of selected Items
+ */
 
 let calculation = () => {
     let cartIcon = document.getElementById("cartAmount");
@@ -20,11 +20,11 @@ let calculation = () => {
 
 calculation();
 
-
-// Generates the Cart Page with product cards composed of
-// images, title, price, buttons, & Total price
-// When basket is blank -> show's Cart is Empty
-
+/**
+ * ! Generates the Cart Page with product cards composed of
+ * ! images, title, price, buttons, & Total price
+ * ? When basket is blank -> show's Cart is Empty
+ */
 
 let generateCartItems = () => {
     if (basket.length !== 0) {
@@ -32,7 +32,7 @@ let generateCartItems = () => {
             .map((x) => {
                 let { id, item } = x;
                 let search = shopItemsData.find((x) => x.id === id) || [];
-                let { img, price, name, } = search;
+                let { img, price, name } = search;
                 return `
       <div class="cart-item">
         <img width="100" src=${img} alt="" />
@@ -75,9 +75,9 @@ let generateCartItems = () => {
 
 generateCartItems();
 
-
-//used to increase the selected product item quantity by 1
-
+/**
+ * ! used to increase the selected product item quantity by 1
+ */
 
 let increment = (id) => {
     let selectedItem = id;
@@ -97,9 +97,9 @@ let increment = (id) => {
     localStorage.setItem("data", JSON.stringify(basket));
 };
 
-
-//used to decrease the selected product item quantity by 1
-
+/**
+ * ! used to decrease the selected product item quantity by 1
+ */
 
 let decrement = (id) => {
     let selectedItem = id;
@@ -117,9 +117,9 @@ let decrement = (id) => {
     localStorage.setItem("data", JSON.stringify(basket));
 };
 
-
-//To update the digits of picked items on each item card
-
+/**
+ * ! To update the digits of picked items on each item card
+ */
 
 let update = (id) => {
     let search = basket.find((x) => x.id === id);
@@ -128,10 +128,10 @@ let update = (id) => {
     TotalAmount();
 };
 
-
-// Used to remove 1 selected product card from basket
-// using the X [cross] button
-
+/**
+ * ! Used to remove 1 selected product card from basket
+ * ! using the X [cross] button
+ */
 
 let removeItem = (id) => {
     let selectedItem = id;
@@ -142,11 +142,11 @@ let removeItem = (id) => {
     localStorage.setItem("data", JSON.stringify(basket));
 };
 
-
-// Used to calculate total amount of the selected Products
-// with specific quantity
-// When basket is blank, it will show nothing
-
+/**
+ * ! Used to calculate total amount of the selected Products
+ * ! with specific quantity
+ * ? When basket is blank, it will show nothing
+ */
 
 let TotalAmount = () => {
     if (basket.length !== 0) {
@@ -168,8 +168,9 @@ let TotalAmount = () => {
 
 TotalAmount();
 
-//Used to clear cart, and remove everything from local storage
-
+/**
+ * ! Used to clear cart, and remove everything from local storage
+ */
 
 let clearCart = () => {
     basket = [];
